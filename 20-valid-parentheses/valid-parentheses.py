74fcf9f0b2af19fa24875a2f4836ce397123271c
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-12-02 20:25:44 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        d = {"}":"{", ")":"(", "]":"["}
+        
+        _dict = {"]":"[", "}":"{", ")":"("}
 
+        stack = []
         for char in s:
-            if char in "{([":
+            if char in "({[":
                 stack.append(char)
             else:
                 if len(stack) == 0:
                     return False
-                # if we haven't returned at this point it means char is a closing
-                # bracket.
-                if d[char] != stack.pop():
+                # we are still valid to process
+                if _dict[char] != stack.pop():
                     return False
-        
         return len(stack) == 0
+        

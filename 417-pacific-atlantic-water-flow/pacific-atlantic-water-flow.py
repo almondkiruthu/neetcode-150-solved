c2f084c0e9b89f440403854ff1147763dee970be
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-01-06 03:47:27 +0200

    Time: 23 ms (93.52%) | Memory: 18.7 MB (97.41%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,44 +1,63 @@
 from collections import deque
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        p_que = deque()
-        p_seen = set()
+        # pacific ocean structures.
+        p_q = deque()
+        p_set = set()
+
+        # Atlantic ocean structures.
+        a_q = deque()
+        a_set = set()
+
+        rows = len(heights)
+        cols = len(heights[0])
+
+        # Get all the coordinates next to the oceans.
+        # Vertical borders (Left = Pacific, Right = Atlantic)
+        for r in range(rows):
+            # Left border (Pacific)
+            p_set.add((r, 0))
+            p_q.append((r, 0))
+            
+            # Right border (Atlantic)
+            a_set.add((r, cols - 1))
+            a_q.append((r, cols - 1))
+            
+        # Horizontal borders (Top = Pacific, Bottom = Atlantic)
+        for c in range(cols):
+            # Top border (Pacific)
+            p_set.add((0, c))
+            p_q.append((0, c))
+            
+            # Bottom border (Atlantic)
+            a_set.add((rows - 1, c))
+            a_q.append((rows - 1, c))
+
+        def get_coords(queue, seen):
+            # Perform the BFS on each position so far.
+            while queue:
+                n = len(queue)
+                for _ in range(n):
+                    i, j = queue.popleft()
+                    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+                    for dr, dc in directions:
+                        n_r = dr + i
+                        n_c = dc + j
+                        # what's the unwanted case?
+                        if (n_r < 0 or n_r >= rows or
+                            n_c < 0 or n_c >= cols or
+                            # We are going upstream therefore neighbor must be of greater
+                            # height than what we have currently.
+                            heights[n_r][n_c] < heights[i][j] or
+                            (n_r, n_c) in seen):
+                            continue
+                        else:
+                            queue.append((n_r, n_c))
+                            seen.add((n_r, n_c))
+
+        get_coords(p_q, p_set)
+        get_coords(a_q, a_set)
+
+        return list(p_set.intersection(a_set))
+
         
-        a_que = deque()
-        a_seen = set()
-        
-        m, n = len(heights), len(heights[0])
-
-        for j in range(n):
-            p_que.append((0, j))
-            p_seen.add((0, j))
-            
-        for i in range(1, m):
-            p_que.append((i, 0))
-            p_seen.add((i, 0))
-            
-        for i in range(m):
-            a_que.append((i, n - 1))
-            a_seen.add((i, n - 1))
-            
-        for j in range(n - 1):
-            a_que.append((m - 1, j))
-            a_seen.add((m - 1, j))
-
-        def get_coords(que, seen):
-            # coords = set()
-            while que:
-                i, j = que.popleft()
-                for i_off, j_off in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    r, c = i + i_off, j + j_off
-                    if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r, c) not in seen:
-                        seen.add((r, c))
-                        que.append((r, c))
-            
-        get_coords(p_que, p_seen)
-        get_coords(a_que, a_seen)
-        return list(p_seen.intersection(a_seen))
-
-# Time Complexity: O(m*n)
-# Space Complexity: O(m*n)

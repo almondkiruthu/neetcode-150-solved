b0026c61f1bb798d022ed7f5aa626a131a927b90
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-01-06 02:32:06 +0200

    Time: 43 ms (54.02%) | Memory: 17.7 MB (89.35%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,5 +9,35 @@ import copy
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        return copy.deepcopy(node)
+        # The pythonic way, which I know the interviewer will not like.
+        # return copy.deepcopy(node)
+        if not node:
+            return None
+        
+        old_to_new = {}
+        visited = set()
+        #dfs function to recreate and map the old to the new.
+        def dfs(node):
+            if node in visited:
+                return
+            else:
+                visited.add(node)
+                old_to_new[node] = Node(node.val)
+                for nei in node.neighbors:
+                    dfs(nei)
+        # Explore all the old nodes
+        dfs(node)
+        
+        #Map the neighbours.
+        for old, new in old_to_new.items():
+            for nei in old.neighbors:
+                # fetch the newly created node, by accessing the old node, that maps
+                # to the new node.
+                new_nei = old_to_new[nei]
+                new.neighbors.append(new_nei)
+
+        return old_to_new[node]
+
+
+        
         

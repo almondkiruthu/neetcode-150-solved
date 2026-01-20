540275b5fb64edba13011fbd4b5225abc0781078
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-01-20 17:33:58 +0200

    Time: 9 ms (59.04%) | Memory: 30.8 MB (5.05%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,23 +1,26 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        windowSum = 0
-        windowStart = 0
+        window_start = 0
+        window_end = 0
         min_length = float("inf")
+        n = len(nums)
 
-        for windowEnd in range(len(nums)):
-            windowSum += nums[windowEnd]
+        window_sum = 0
+        while window_end < n:
+            window_sum += nums[window_end]
 
-            while windowSum >= target:
-                windowSize = windowEnd - windowStart + 1
-                min_length = min(min_length, windowSize)
-                windowSum -= nums[windowStart]
-                windowStart += 1
-        
-        # We check if our minimum is still what we started with return zero
+            while window_sum >= target:
+                # I have found my target, can we shrink? And move forward to see
+                # If we can find a larger one?
+                window_size = window_end - window_start + 1
+                min_length = min(min_length, window_size)
+                window_sum -= nums[window_start]
+                window_start += 1
+            
+            window_end += 1
+
+
         if min_length == float("inf"):
             return 0
         else:
             return min_length
-       
-
-        

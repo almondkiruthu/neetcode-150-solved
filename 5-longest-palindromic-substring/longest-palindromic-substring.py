25b6a59f0ce5282aa2a69a6fc4d360d3aa855546
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-02-25 23:40:44 +0200

    Time: 330 ms (42.10%) | Memory: 19.4 MB (36.27%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,23 +1,43 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # self.res = ""
+
+        # def find(i, j, s):
+        #     if j == len(s):
+        #         return
+
+        #     curr_str = s[i:j+1]
+
+        #     if curr_str == curr_str[::-1]:
+        #         if len(self.res) < len(curr_str):
+        #             self.res = curr_str
+
+        #     find(i, j + 1, s)
+
+        # n = len(s)
+        # for i in range(len(s)):
+        #     find(i, i, s)
+
+        # return self.res
+
         res = ""
-        res_len = 0
         for i in range(len(s)):
-            # odd length
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r - l + 1
-                l -= 1
-                r += 1
-            # even length
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r - l + 1
-                l -= 1
-                r += 1
-        
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(res):
+                    res = s[left:right + 1]
+
+                left -= 1
+                right += 1
+
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(res):
+                    res = s[left:right + 1]
+
+                left -= 1
+                right += 1
+
         return res
+            
+

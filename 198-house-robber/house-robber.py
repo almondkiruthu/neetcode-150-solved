c28b66e4635c6643a82db437e1a38df5faf70728
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-07-13 17:41:36 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.95%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,19 +2,16 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         dp = {}
         N = len(nums)
-        def fn(i):
-            # we want to reach the last step, so we do inclusive.
-            if i >= N:
-                return 0
-            if i in dp:
-                return dp[i]
+        # Tabulation we begin from the basecase
+        dp[N] = 0
+        dp[N + 1] = 0
+        dp[N + 2] = 0
+        for i in range(N - 1, -1, -1):
+            pick = nums[i] + dp[i + 2]
+            not_pick = 0 + dp[i + 1]
 
-            reward = float("-inf")
-                                    #pick the element       #don't pick
-            reward = max(reward, nums[i] + fn(i + 2), 0 + fn(i + 1))
-            dp[i] = reward
-            return reward
-        
-        return fn(0)
+            dp[i] = max(pick, not_pick)
+
+        return dp[0]
         
         

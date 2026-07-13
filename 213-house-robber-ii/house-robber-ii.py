024bee5f70b151aeb164c7e75f77eb12ade844df
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-07-13 18:33:27 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,22 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
         if len(nums) == 1:
             return nums[0]
         
-        N = len(nums)
-        def fn(i, n, dp):
-            if i >= n + 1:
-                return 0
-            if i in dp:
-                return dp[i]
-
-            pick = nums[i] + fn(i + 2, n, dp)
-            not_pick = 0 + fn(i + 1, n, dp)
-
-            dp[i] = max(pick, not_pick)
+        # Let's tabulate
+        def fn_tabulate(arr):
+            if len(arr) == 1:
+                return arr[0]
+            N = len(arr)
+            dp = {}
+            dp[N] = 0
+            dp[N + 1] = 0                     
+            for i in range(N - 1, -1, -1):
+                pick = arr[i] + dp[i + 2]
+                not_pick = 0 + dp[i + 1]
+                dp[i] = max(pick, not_pick)
             
-            return dp[i]
+            return dp[0]
 
-        return max(fn(0, N - 2, {}), fn(1, N - 1, {}))
+        first_pass = fn_tabulate(nums[:-1])
+        second_pass = fn_tabulate(nums[1:])
+
+        return max(first_pass, second_pass)
+        
         

31bcd50d7d0a0a8a0db5e2ef45124d779590e627
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2026-07-14 13:48:55 +0300

    Time: 39 ms (12.74%) | Memory: 25.7 MB (10.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,23 +1,26 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = {}
         m = len(grid)
         n = len(grid[0])
-        def fn(i, j):
-            if i == 0 and j == 0:
-                return grid[i][j]
-            if i < 0 or j < 0:
-                return float("inf")
-            if (i, j) in dp:
-                return dp[(i, j)]
-            
-            # do stuffs on the index.
-            # invert bottom -> up and right -> left
-            up = grid[i][j] + fn(i - 1, j)
-            left = grid[i][j] + fn(i, j - 1)
 
-            dp[(i, j)] = min(up, left)
+        dp = {}
 
-            return min(up, left)
+        # tabulation
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[(0, 0)] = grid[i][j]
+                    continue
 
-        return fn(m - 1, n - 1)
+                # do stuffs on the index.
+                # invert bottom -> up and right -> left
+                up = float("inf")
+                left = float("inf")
+                if i - 1 >= 0:
+                    up = grid[i][j] + dp[(i - 1, j)]
+                if j - 1 >= 0:
+                    left = grid[i][j] + dp[(i, j - 1)]
+
+                dp[(i, j)] = min(up, left)
+
+        return dp[(m - 1, n - 1)]
